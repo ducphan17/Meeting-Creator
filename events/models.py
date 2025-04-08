37a3aee5df2218193915400e5e66d36c3d3c42bf
SriramL1 <srir5182@gmail.com>
2025-04-08 22:44:33 +0000

    Added Event and Availability models with test data

--- events/models.py
+++ events/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_events')
+    passcode = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    participants = models.ManyToManyField(User, related_name='participating_events', blank=True)
+
+    class Meta:
+        db_table = 'events'
+
+class Availability(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='availabilities')
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    class Meta:
+        db_table = 'availabilities'
